34badc320e754b8a26c7197af61a1f6ed39d8383
Jake Rhodes <jakerhodes8@gmail.com>
2023-10-26 17:43:21 -0600

    working unit test for sum_to_one

--- my_package/helpers.py
+++ my_package/helpers.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def sum_to_one(x, axis = 1):
+def sum_to_one(x, axis = 0):
     """
     Parameters
     ----------
@@ -14,11 +14,11 @@ def sum_to_one(x, axis = 1):
     -------
     row_normalized_array : ndarray
         An array with the same shape as `x`, with the specified
-        axis removed. The array is normalized so the rows (axis = 0) or columns (axis = 1) sum to 1.
+        axis removed. The array is normalized so the rows (axis = 1) or columns (axis = 0) sum to 1.
 
     Examples
     --------
     >>> sum_to_one(np.array([[1, 2], [3, 4]]))
     np.array([[.333333, .666667], [0.42857, 0.57142]])
     """
-    return x / np.sum(x, axis = axis)
+    return x / x.sum(axis = axis, keepdims = True)
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,10 @@
-from ..my_package import helpers
+from my_package import helpers
 import numpy as np
 
-def test_sum_to_one():
+def test_sum_to_one_row():
     x = np.array([[1, 1], [1, 3]])
-    assert helpers.sum_to_one(x) == np.array([[.5, .5], [.25, .75]])
+    assert (helpers.sum_to_one(x, axis = 1) == np.array([[.5, .5], [.25, .75]])).all()
+
+def test_sum_to_one_column():
+    x = np.array([[1, 1], [1, 3]])
+    assert (helpers.sum_to_one(x, axis = 0) == np.array([[.5, .25], [.5, .75]])).all()
