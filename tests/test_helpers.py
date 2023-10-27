8d12adb8ac0024be0941f97adc5c3f6afb6ed6a4
Jake Rhodes <jakerhodes8@gmail.com>
2023-10-27 09:06:55 -0600

    add documentation for test functions.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -4,9 +4,15 @@ from my_package import helpers
 import numpy as np
 
 def test_sum_to_one_row():
+    """
+    Test that rows are normalized properly.
+    """
     x = np.array([[1, 1], [1, 3]])
     assert (helpers.sum_to_one(x, axis = 1) == np.array([[.5, .5], [.25, .75]])).all()
 
 def test_sum_to_one_column():
+    """
+    Test that columns are normalized properly.
+    """
     x = np.array([[1, 1], [1, 3]])
     assert (helpers.sum_to_one(x, axis = 0) == np.array([[.5, .25], [.5, .75]])).all()
