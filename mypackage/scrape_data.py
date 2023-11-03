abc274f685604254d64ac6951373974388d3ecc7
Jake Rhodes <jakerhodes8@gmail.com>
2023-11-03 06:48:24 -0600

    add documentation

--- mypackage/scrape_data.py
+++ mypackage/scrape_data.py
@@ -1,15 +1,19 @@
+"""
+This script performs the webscraping and produces the raw dataset to be cleaned.
+"""
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import json
 
-##baseurl
 url = 'https://www.churchofjesuschrist.org/temples/list?lang=eng'
 r = requests.get(url)
 soup = BeautifulSoup(r.content)
 json_data  = soup.find(id = '__NEXT_DATA__').string
 temple_dict = json.loads(json_data)
+
 temple_list = temple_dict['props']['pageProps']['templeList']
 temple_df = pd.DataFrame(temple_list)
 
-temple_df.to_csv('data/temples_raw.csv')
+temple_df.to_csv('mypackage/data/temples_raw.csv', index = False)
