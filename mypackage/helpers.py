cb85021e7e267628e821fafc30aeec646f78d3aa
Jake Rhodes <jakerhodes8@gmail.com>
2023-11-03 06:48:01 -0600

    lint and add documentation

--- mypackage/helpers.py
+++ mypackage/helpers.py
@@ -1,24 +1,24 @@
-import numpy as np
-
-def sum_to_one(x, axis = 0):
+def sum_to_one(x, axis=0):
     """
+    Normalize x so that the rows or columns sum to one.
+
     Parameters
     ----------
         x : array_like
         Array to normalize
-    axis : int, optional
-        Axis or axes along which a normalization is performed.  The default,
-        axis = 1, will row-normalize x.
+        axis : int, optional
+        Axis or axes along which a normalization is performed.        The default, axis = 1, will row-normalize x.
 
     Returns
     -------
-    row_normalized_array : ndarray
+        row_normalized_array : ndarray
         An array with the same shape as `x`, with the specified
-        axis removed. The array is normalized so the rows (axis = 1) or columns (axis = 0) sum to 1.
+        axis removed. The array is normalized so the rows (axis = 1) or
+        columns (axis = 0) sum to 1.
 
     Examples
     --------
     >>> sum_to_one(np.array([[1, 2], [3, 4]]))
     np.array([[.333333, .666667], [0.42857, 0.57142]])
     """
-    return x / x.sum(axis = axis, keepdims = True)
+    return x / x.sum(axis=axis, keepdims=True)
