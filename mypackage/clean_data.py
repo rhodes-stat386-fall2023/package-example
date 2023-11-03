5d878f41646aac8ab8b8b3127d0425120e9f8213
Jake Rhodes <jakerhodes8@gmail.com>
2023-11-03 06:47:42 -0600

    add script to clearn raw data

--- mypackage/clean_data.py
+++ mypackage/clean_data.py
@@ -1,1 +1,11 @@
-# TODO: Create a script to import data, clean it, and export the clean data.
+"""
+This script imports the raw data, cleans it, and explorts the final dataset.
+"""
+
+import pandas as pd
+
+
+temple_df = pd.read_csv('mypackage/data/temples_raw.csv')
+temple_df['dateTime'] = pd.to_datetime(temple_df['sortDate'])
+temple_df = temple_df.sort_values('dateTime')
+temple_df.to_csv('mypackage/data/temples.csv', index = False)
