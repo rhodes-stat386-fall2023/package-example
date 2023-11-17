db0ee8653add5d852c8acff9151ee5d4600e3d75
Jake Rhodes <jakerhodes8@gmail.com>
2023-11-17 16:23:15 -0700

    add if __name__ ==..

--- mypackage/clean_data.py
+++ mypackage/clean_data.py
@@ -2,12 +2,13 @@
 This script imports the raw data, cleans it, and explorts the final dataset.
 """
 
-import pandas as pd
+if __name__ == '__main__':
+    import pandas as pd
 
 
-temple_df = pd.read_csv('mypackage/data/temples_raw.csv')
-temple_df['dateTime'] = pd.to_datetime(temple_df['sortDate'])
-temple_df = temple_df.sort_values('dateTime')
-temple_df.to_csv('mypackage/data/temples.csv', index = False)
+    temple_df = pd.read_csv('mypackage/data/temples_raw.csv')
+    temple_df['dateTime'] = pd.to_datetime(temple_df['sortDate'])
+    temple_df = temple_df.sort_values('dateTime')
+    temple_df.to_csv('mypackage/data/temples.csv', index = False)
 
 # Ideally, you would use some of your custom functions as a part of the clearning.
--- mypackage/scrape_data.py
+++ mypackage/scrape_data.py
@@ -2,18 +2,19 @@
 This script performs the webscraping and produces the raw dataset to be cleaned.
 """
 
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-import json
+if __name__ == '__main__':
+    import pandas as pd
+    import requests
+    from bs4 import BeautifulSoup
+    import json
 
-url = 'https://www.churchofjesuschrist.org/temples/list?lang=eng'
-r = requests.get(url)
-soup = BeautifulSoup(r.content)
-json_data  = soup.find(id = '__NEXT_DATA__').string
-temple_dict = json.loads(json_data)
+    url = 'https://www.churchofjesuschrist.org/temples/list?lang=eng'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content)
+    json_data  = soup.find(id = '__NEXT_DATA__').string
+    temple_dict = json.loads(json_data)
 
-temple_list = temple_dict['props']['pageProps']['templeList']
-temple_df = pd.DataFrame(temple_list)
+    temple_list = temple_dict['props']['pageProps']['templeList']
+    temple_df = pd.DataFrame(temple_list)
 
-temple_df.to_csv('mypackage/data/temples_raw.csv', index = False)
+    temple_df.to_csv('mypackage/data/temples_raw.csv', index = False)
